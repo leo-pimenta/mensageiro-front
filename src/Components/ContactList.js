e30a597bc188604629e55a9fa4f3cc68ca32670f
leo.dsn.pack <leo.dsn.pack@alterdata.com.br>
2021-08-28 19:21:15 -0300

    contacts list creation and filter

--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,37 +1,61 @@
 import React, { useEffect } from 'react'
-import { Grid, Avatar }  from '@material-ui/core'
+import { Grid, Avatar, TextField } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import service from '../service/contactService'
 
 const useStyle = makeStyles({
     contactElement: {
-        marginTop: '0.5rem'
+        cursor: 'pointer',
+        marginTop: '0.5rem',
+        '&:hover': {
+            backgroundColor: '#000000'
+        }
     },
     elementParagraph: {
         margin: '0',
         padding: '0'
+    },
+    messageContainer: {
+        whiteSpace: 'nowrap',
+        overflow: 'hidden'
+    },
+    contactInfos: {
+        maxWidth: '80%'
+    },
+    findContactsInput: {
+        width: '100%'
+    },
+    homeContainer: {
+        marginLeft: '0.5rem'
     }
 });
 
 function ContactList () {
     const [contacts, setContacts] = React.useState([]);
+    const [filteredContacts, setFilteredContacts] = React.useState([]);
     const [body, setBody] = React.useState(undefined);
     const classes = useStyle();
 
     function createContactsList() {
-        return contacts.map(createContactElement);
+        return filteredContacts.map(createContactElement);
+    }
+
+    function onContactsFilterInput (e) {
+        const text = e?.target?.value.toLowerCase();
+        const filteredContacts = !text ? contacts : contacts.filter(key => key.contact.nickname.toLowerCase().includes(text));
+        setFilteredContacts(filteredContacts);
     }
 
     function createContactElement(contact) {
         let contactObj = contact.contact;
         
         return (
-            <Grid container item alignItems='center' spacing='1' className={classes.contactElement}>
+            <Grid container item direction='row' alignItems='center' spacing='1' className={classes.contactElement}>
                 <Grid item>
                     <Avatar></Avatar>
                 </Grid>
 
-                <Grid item>
+                <Grid item className={classes.contactInfos}>
                     <Grid item>
                         <p className={classes.elementParagraph}>
                             <strong>
@@ -40,9 +64,9 @@ function ContactList () {
                         </p>
                     </Grid>
 
-                    <Grid item>
+                    <Grid item className={classes.messageContainer}>
                         {/* // TODO put real message here */}
-                        <p className={classes.elementParagraph}>This was my last message to you</p>
+                        <p className={classes.elementParagraph}>This was my last message to you. This message should be too long to appear in heeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeere'</p>
                     </Grid>
                 </Grid>
             </Grid>
@@ -50,15 +74,25 @@ function ContactList () {
     }
     
     useEffect(() => { 
-        const contactsList = createContactsList();
-        setBody(contactsList);
+        onContactsFilterInput();
     }, [contacts])
 
-    useEffect(() => service.getContacts().then(contactsReceived => setContacts(contactsReceived)), [])
+    useEffect(() => {
+        const contactsList = createContactsList();
+        setBody(contactsList);
+    }, [filteredContacts]);
+
+    useEffect(() => service.getContacts().then(contactsReceived => setContacts(contactsReceived)), []);
     
     return (
-        <Grid container>
-            {body}
+        <Grid container className={classes.homeContainer}>
+            <Grid container item>
+                <TextField className={classes.findContactsInput} label='Find contact...' onInput={onContactsFilterInput}></TextField>
+            </Grid>
+
+            <Grid container item>
+                {body}
+            </Grid>
         </Grid>
     );
 }
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,14 +1,73 @@
-import { Grid } from '@material-ui/core'
+import React from 'react'
+import { Grid, useMediaQuery, Drawer, Button } from '@material-ui/core'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Chat from '@material-ui/icons/Chat'
 import Header from '../Header'
 import ContactList from '../ContactList';
+import userService from '../../service/userService';
+
+const useStyle = makeStyles({
+    contactsButton: {
+        position: 'fixed',
+        height: '3rem',
+        backgroundColor: '#5f5f5f'
+    },
+    chatIconContainer: {
+        paddingTop: '0.4rem',
+        marginRight: '0.2rem'
+    }
+});
 
 function Home(props) {
     const onLogout = props.onLogout ?? function () {};
+    const theme = useTheme();
+    const isSmall = useMediaQuery(theme.breakpoints.down('md'));
+    const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+    const classes = useStyle();
+
+    function getContactList() {
+        if (isSmall) {
+            return (
+                <div>
+                    <Grid 
+                        container 
+                        item 
+                        className={classes.contactsButton} 
+                        onClick={() => setIsDrawerOpen(true)}
+                        direction='row'
+                        alignItems='center'
+                        justifyContent='center'
+                    >
+                        <Grid item className={classes.chatIconContainer}><Chat></Chat></Grid>
+                        <Grid item>Contacts</Grid>
+                    </Grid>
+
+                    <Drawer anchor='top' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
+                        <ContactList></ContactList>
+                    </Drawer>
+                </div>
+            );
+        }
+        else {
+            return (
+                <Grid item xs='3'>
+                    <ContactList></ContactList>
+                </Grid>
+            );
+        }
+    }
 
     return (
         <Grid container spacing='0' direction='column'>
             <Header onLogout={onLogout}></Header>
-            <ContactList></ContactList>
+            
+            <Grid container item spacing='2'>
+                { getContactList() }
+
+                <Grid>
+                    <p>Chat...</p>
+                </Grid>
+            </Grid>            
         </Grid>
     );
 }
